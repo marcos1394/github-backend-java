66e36b634db58e908b1a8d2d609b3d3eab8350c3
ingsandoval1394 <ingsandoval1394@gmail.com>
2026-01-17 01:33:12 +0000

    se forza a usar perfil de test en pruebas y adicional se agrega herramientas de observabilidad y monitoreo

--- src/test/java/com/quhealthy/auth_service/AuthServiceApplicationTests.java
+++ src/test/java/com/quhealthy/auth_service/AuthServiceApplicationTests.java
@@ -2,12 +2,17 @@ package com.quhealthy.auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // <--- ESTA LÍNEA ES LA SOLUCIÓN. Obliga a usar application-test.properties
 class AuthServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Este test verifica que Spring Boot pueda levantar (inyección de dependencias, 
+        // conexión a BD en memoria, lectura de configs) sin explotar.
+        // Si pasa esto, el Deploy es seguro.
+    }
 
 }
