e6832d6daa6f0f2d448a9b4b57e159573497b858
ingsandoval1394 <ingsandoval1394@gmail.com>
2026-01-24 20:45:29 +0000

    pequeña correccion de securityconfig

--- social_service/src/main/java/com/quhealthy/social_service/SocialServiceApplication.java
+++ social_service/src/main/java/com/quhealthy/social_service/SocialServiceApplication.java
@@ -2,10 +2,13 @@ package com.quhealthy.social_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@EnableScheduling // 👈 Vital para que funcionen los Cron Jobs de publicación
+// 1. exclude: Evita que Spring cree un usuario/password por defecto en consola.
+// 2. EnableScheduling: Activa el Cron Job para publicar posts automáticamente.
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
+@EnableScheduling 
 public class SocialServiceApplication {
 
     public static void main(String[] args) {
