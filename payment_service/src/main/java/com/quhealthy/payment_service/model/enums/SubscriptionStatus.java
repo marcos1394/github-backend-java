dfcdf8ed42d48f36ad553108e7a4833f59e25b2c
ingsandoval1394 <ingsandoval1394@gmail.com>
2026-01-26 02:27:01 +0000

    correccion de enum

--- payment_service/src/main/java/com/quhealthy/payment_service/model/enums/SubscriptionStatus.java
+++ payment_service/src/main/java/com/quhealthy/payment_service/model/enums/SubscriptionStatus.java
@@ -3,7 +3,8 @@ package com.quhealthy.payment_service.model.enums;
 public enum SubscriptionStatus {
     ACTIVE,         // Todo bien, pagado y funcionando
     PAST_DUE,       // Falló el pago, estamos reintentando (Gracia)
-    CANCELED,       // El usuario canceló o se agotaron los intentos de cobro
+    CANCELED,
+    PENDING,       // El usuario canceló o se agotaron los intentos de cobro
     UNPAID,         // Factura final no pagada
     TRIALING,       // Periodo de prueba gratis
     INCOMPLETE      // Se creó la intención pero no se completó el pago inicial
